dff29fe96e78630fc19efd9622a799aa3e34df31
Pratik Kumar <67475692+ghostinlinux@users.noreply.github.com>
2024-10-15 14:20:15 +0530

    Update basetest.ts

--- libs/basetest.ts
+++ libs/basetest.ts
@@ -1,19 +1,22 @@
 import { test as baseTest, expect, type Page } from "@playwright/test";
 import { measureExecutionTime } from "sharding-pk-play";
 
-export const test = baseTest.extend({
-  // Define global beforeEach hook
-  page: async ({ page }, use, testInfo) => {
-    await page.goto("https://demo.playwright.dev/todomvc");
-    testInfo.duration; // Access test duration if needed
-    await use(page);
-  },
+// Extend the base test with custom beforeEach and afterEach
+export const test = baseTest.extend({});
+
+// Define a global beforeEach hook
+test.beforeEach(async ({ page }, testInfo) => {
+  console.log(`Running beforeEach for ${testInfo.title} Before Each`);
+  await page.goto('https://demo.playwright.dev/todomvc');
+  testInfo.duration
 });
 
-// Global afterEach hook
+// Define a global afterEach hook
 test.afterEach(async ({ page }, testInfo) => {
+  console.log(`Running afterEach for ${testInfo.title} After Each`);
   measureExecutionTime(testInfo); // Custom function to measure time
 });
 
+// Export the custom test object and expect for use in other test files
 export { expect };
 export { Page };
